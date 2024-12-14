8619a3a3d181bbc000e77da20b7ff80b3f158558
zero <green.sadir95@gmail.com>
2024-12-13 23:00:35 -0500

    deploying react app

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/vite-react-deploy",
   build: {
     outDir: 'build' // Ensure output is in the 'build' directory
   }
