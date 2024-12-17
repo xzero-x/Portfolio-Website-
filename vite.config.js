7276d0c3b9d6fb4a75082a218d8188a3c05a0ab2
zero <green.sadir95@gmail.com>
2024-12-16 22:40:52 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/website-portfolio",
+  base: "/Portfolio-website",
   build: {
     outDir: 'build' // Ensure output is in the 'build' directory
   }
