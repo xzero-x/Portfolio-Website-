ef4f7974b6e38e420e8a15b96d6ffc646bd1d048
zero <green.sadir95@gmail.com>
2024-12-16 22:34:02 -0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy",
+  base: "/website-portfolio",
   build: {
     outDir: 'build' // Ensure output is in the 'build' directory
   }
